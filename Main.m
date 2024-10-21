aa07631cf9465427b75dd33a79695480e9b34bb0
Cooper Buch <cbuch690@gmail.com>
2024-10-21 14:07:55 -0700

    implemented blue path

--- Main.m
+++ Main.m
@@ -6,22 +6,17 @@ disp('EV3 Brick found');
 
 
 %------------------------------------  Parameters  --------------------------------------
-distance_Threshold = 20; %how close the robot can be to a wall before turning
-
 left_motor_port = 'A'; %port that the left motor is connected to
 
 right_motor_port = 'B'; %port that the right motor is connected to
 
-ultrasonic_sensor_port = 1; %port that the ultrasonic sensor is connected to
-
-turning_degrees = 90; %angle in degrees to turn the left motor to make the car turn at a right angle
+touch_sensor_port = 1; %port that the touch sensor is connected to
 
 color_sensor_port = 2; %port that the color sensor is connected to
 
-color_sensor_mode = 4;
+turning_degrees = 110; %the angle in degrees to turn the left motor to make the car turn at a right angle
 %----------------------------------------------------------------------------------------
 
-
-car = Vantage(ev3Brick, left_motor_port, right_motor_port, ultrasonic_sensor_port, color_sensor_port, distance_Threshold, turning_degrees, color_sensor_mode);
+car = Vantage(ev3Brick, left_motor_port, right_motor_port, touch_sensor_port, color_sensor_port, turning_degrees);
 
 car.run();
--- Vantage.m
+++ Vantage.m
@@ -3,47 +3,85 @@ classdef Vantage
         ev3Brick
         left_motor_port %Port that the left motor is connected to
         right_motor_port %Port that the right motor is connected to
-        ultrasonic_sensor_port %Port that the ultrasonic sensor is connected to
+        touch_sensor_port %Port that the ultrasonic sensor is connected to
         color_sensor_port %Port that the color sensor is connected to
-        distance_threshold %closest distance the car can be from a wall before turning
         turning_degrees %Amount of degrees to turn the motor to rotate the car 90 degrees
     end
 
     methods(Access = public) %Public methods
-        function obj = Vantage(ev3Brick, left_motor_port, right_motor_port, ultrasonic_sensor_port, color_sensor_port, distance_threshold, turning_degrees, color_mode)
-            ev3Brick.SetColorMode(color_sensor_port, color_mode);
+        function obj = Vantage(ev3Brick, left_motor_port, right_motor_port, touch_sensor_port, color_sensor_port, turning_degrees)
+            ev3Brick.SetColorMode(color_sensor_port, 4);
             obj.ev3Brick = ev3Brick;
             obj.left_motor_port = left_motor_port;
             obj.right_motor_port = right_motor_port;
-            obj.ultrasonic_sensor_port = ultrasonic_sensor_port;
+            obj.touch_sensor_port = touch_sensor_port;
             obj.color_sensor_port = color_sensor_port;
-            obj.distance_threshold = distance_threshold;
             obj.turning_degrees = turning_degrees;
         end
 
         function run(obj)
-            %main loop
-            obj.startMotors();
-            
-            while true
-
-                if obj.is_at_red_light()
-                    %Stop the motors
-                    obj.stopMotors();
-                    pause(5);
-                    obj.startMotors();
-                    while obj.is_at_red_light()
-                        pause(0.05)
-                    end
-                end
-
-
-
+            if obj.is_on_blue_square()
+                obj.run_blue_path();
+            elseif obj.is_on_green_square();
+                obj.run_green_path();
             end
         end
     end
 
     methods(Access = private) %Private methods
+        function run_green_path(obj)
+            %not yet implemented
+        end
+
+        function run_blue_path(obj)
+            wall_number = 0;
+            obj.startMotors();
+            while true
+                if obj.is_touching_wall()
+                    if wall_number == 0
+                        obj.rotate_right();
+                        wall_number = wall_number + 1;
+                    elseif wall_number == 1
+                        obj.rotate_left();
+                        wall_number = wall_number + 1;
+                    elseif wall_number == 2
+                        obj.rotate_right();
+                        wall_number = wall_number + 1;
+                    elseif wall_number == 3
+                        obj.rotate_left();
+                        wall_number = wall_number + 1;
+                    elseif obj.is_at_red_light()
+                        obj.stop_at_red_light();
+                        %wait 1 second for the robot to pass the red light
+                        pause(1);
+                        obj.rotate_left();
+                    elseif wall_number == 4
+                        obj.rotate_right();
+                        wall_number = wall_number + 1;
+                    elseif wall_number == 5
+                        obj.rotate_left();
+                        wall_number = wall_number + 1;
+                    elseif wall_number == 6
+                        obj.rotate_left();
+                        wall_number = wall_number + 1;
+                    elseif obj.is_on_yellow_square()
+                        obj.stopMotors();
+                        break;
+                    end
+                end
+            end
+        end
+
+
+        function stop_at_red_light(obj)
+            obj.stopMotors();
+            pause(5);
+            obj.startMotors();
+            while obj.is_at_red_light()
+                %dont exit the loop until the robot has move passed the "red light"
+            end
+        end
+
         function startMotors(obj)
             obj.ev3Brick.MoveMotor(obj.left_motor_port, 50);
             obj.ev3Brick.MoveMotor(obj.right_motor_port, 50);
@@ -54,10 +92,8 @@ classdef Vantage
             obj.ev3Brick.MoveMotor(obj.right_motor_port, 0);
         end
 
-        function result = did_hit_wall(obj)
-            %change to touch sensor
-            %currentDist = obj.ev3Brick.UltrasonicDist(obj.ultrasonic_sensor_port);
-            if currentDist < obj.distance_threshold
+        function result = is_touching_wall(obj)
+            if obj.ev3Brick.TouchPressed(obj.touch_sensor_port) == 1
                 result = true;
             else
                 result = false;
@@ -65,21 +101,53 @@ classdef Vantage
         end
 
         function rotate_left(obj)
+            obj.stopMotors();
             obj.ev3Brick.MoveMotorAngleRel(obj.right_motor_port, 50, obj.turning_degrees, 'Brake');
+            obj.startMotors();
         end
 
         function rotate_right(obj)
+            obj.stopMotors();
             obj.ev3Brick.MoveMotorAngleRel(obj.left_motor_port, 50, obj.turning_degrees, 'Brake');
+            obj.startMotors();
         end
 
         function result = is_at_red_light(obj)
             [r, g, b] = obj.get_RGB_Colors();
-            if r > 200 && g < 100 && b < 50 
+            if r > 200 && g < 100 && b < 50
                 result = true;
             else
                 result = false;
             end
         end
+
+        function result = is_on_yellow_square(obj)
+            [r, g, b] = obj.get_RGB_Colors();
+            if r > 200 && g > 200 && b < 50
+                result = true;
+            else
+                result = false;
+            end
+        end
+
+        function result = is_on_blue_square(obj)
+            [r, g, b] = obj.get_RGB_Colors();
+            if r < 50 && g < 50 && b > 200
+                result = true;
+            else
+                result = false;
+            end
+        end
+
+        function result = is_on_green_square(obj)
+            [r, g, b] = obj.get_RGB_Colors();
+            if r < 50 && g > 200 && b < 50
+                result = true;
+            else
+                result = false;
+            end
+        end
+
 
         function [red, green, blue] = get_RGB_Colors(obj)
             color_rgb = obj.ev3Brick.ColorRGB(obj.color_sensor_port);
