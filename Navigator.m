75d8777f60444343c2bee4db269659cf3730dd81
Cooper Buch <cbuch690@gmail.com>
2024-10-30 16:02:25 -0700

    e

--- Navigator.m
+++ Navigator.m
@@ -10,12 +10,16 @@ classdef Navigator < Robot
         function run(obj)
             while ~obj.is_on_color("Yellow")
                 if obj.path_ahead_is_clear()
+                    disp('Path ahead is clear\n');
                     obj.move_to_next_wall();
                 elseif obj.path_to_right_is_clear()
+                    disp('Path to right is clear\n');
                     obj.rotate_right();
                 elseif obj.path_to_left_is_clear()
+                    disp('Path to left is clear\n');
                     obj.rotate_left();
                 else
+                    disp('Turning around\n');
                     obj.turn_around();
                 end
                 pause(0.5);
